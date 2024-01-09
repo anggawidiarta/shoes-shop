888b82498ac150a154d39698f27d2d32210319bc
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-09 13:53:58 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import Hero from "./sections/Hero";
+import Services from "./sections/Services";
+import PopularProducts from "./sections/PopularProducts";
+import Footer from "./sections/Footer";
+import SuperQuality from "./sections/SuperQuality";
+import Subscribe from "./sections/Subscribe";
+import CustomerReviews from "./sections/CustomerReviews";
+import SpecialOffer from "./sections/SpecialOffer";
+
 export default function App() {
-  return <h1 className="title text-primary font-palanquin">Hello world!</h1>;
+  return (
+    <main className="relative">
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">CustomerReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="padding-x padding-t bg-black pb-8">Footer</section>
+    </main>
+  );
 }
---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const CustomerReviews = () => {
+  return <div>CustomerReviews</div>;
+};
+
+export default CustomerReviews;
