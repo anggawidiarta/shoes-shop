6e943c1bec1373b30541badbe1bbb2455140f372
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-11 10:12:16 +0800

    feat: add hero section and customize it

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,13 @@
 const Hero = () => {
-  return <div></div>;
+  return (
+    <>
+      <section
+        id="home"
+        className="w-full border-2 border-red-500 flex xl:flex-row min-h-screen justify-center gap-10 max-container p-2"
+      ></section>
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-8"></div>
+    </>
+  );
 };
 
 export default Hero;
