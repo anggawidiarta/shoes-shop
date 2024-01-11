85a80991cdaa943973b81b009a553f698f9b7ac8
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-11 10:14:32 +0800

    feat: add button components and put it in hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,23 @@
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
   return (
     <>
       <section
         id="home"
-        className="w-full border-2 border-red-500 flex xl:flex-row min-h-screen justify-center gap-10 max-container p-2"
-      ></section>
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-8"></div>
+        className="w-full border-2 flex xl:flex-row min-h-screen justify-center gap-10 max-container p-2"
+      >
+        <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+          <p>Our Collection</p>
+          <h1>
+            <span>The New Arrival</span>
+            <br />
+            <span>Nike</span>
+            Shoes
+          </h1>
+          <p>Temukan Gaya Berpakaian Paling Keren Bagi Anda Disini</p>
+        </div>
+      </section>
     </>
   );
 };
