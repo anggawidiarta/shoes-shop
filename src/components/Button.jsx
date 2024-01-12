5d3ab26afe6cb4f60faadb78d00dedd9ad6c6d9b
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-12 22:58:03 +0800

    fix: fixing some attribute in button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({ label, iconUrl }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red text-white rounded-full border-coral-red">
+    <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red">
       {label}
-      <img src={iconUrl} alt="Icon URL" className="ml-2 rounded-full w-5 h-5" />
+      <img src={iconUrl} alt="Icon URL" className="w-5 h-5 ml-2 rounded-full" />
     </button>
   );
 };
