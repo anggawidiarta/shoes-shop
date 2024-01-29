5996f99f3fb69d9f0276d63cd97d8cb500fda7b4
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-29 23:24:24 +0800

    refactor: add condition if the components props doesnt have value

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,13 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red">
       {label}
-      <img src={iconUrl} alt="Icon URL" className="w-5 h-5 ml-2 rounded-full" />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="Icon URL"
+          className="w-5 h-5 ml-2 rounded-full"
+        />
+      )}
     </button>
   );
 };
