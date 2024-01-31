6b5794dcf5d339467ebbba23ef540a8042fd3fce
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-31 23:15:10 +0800

    refactor: just refactor

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,27 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+    } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
+
       {iconUrl && (
         <img
           src={iconUrl}
-          alt="Icon URL"
-          className="w-5 h-5 ml-2 rounded-full"
+          alt="arrow right icon"
+          className="w-5 h-5 ml-2 bg-white rounded-full"
         />
       )}
     </button>
