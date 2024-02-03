ade3fc0b0ba0ab341ff083442bd91140864d2ffd
anggawidiarta <anggawidiarta55@gmail.com>
2024-02-03 13:30:47 +0800

    style: changing bg color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,7 +12,7 @@ const Button = ({
     ${
       backgroundColor
         ? `${backgroundColor} ${textColor} ${borderColor}`
-        : "bg-coral-red text-white border-coral-red"
+        : "bg-secondary text-[#F7F7F7] border-secondary"
     } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
