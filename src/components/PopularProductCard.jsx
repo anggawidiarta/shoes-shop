afcaf0c0e5970e5ea11df11e064346dcf0360623
anggawidiarta <anggawidiarta55@gmail.com>
2024-02-07 10:42:00 +0800

    chore: input new properties to object products

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,20 +1,24 @@
 import { star } from "../assets/icons";
 
-const PopularProductCard = ({ imgURL, name, price }) => {
+const PopularProductCard = ({ imgURL, name, price, rating = "4" }) => {
   return (
     <>
-      <div className="flex flex-col flex-1 w-full max-sm:w-full">
-        <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="flex flex-col flex-1 w-full max-sm:w-full max-sm:justify-center max-sm:items-center ">
+        <img
+          src={imgURL}
+          alt={name}
+          className="w-[280px] h-[280px] md:h-[200px] md:w-[200px] lg:h-[250px] lg:w-[250px] rounded-xl"
+        />
         <div className="mt-8 flex justify-start gap-2.5">
           <img src={star} alt="rating" width={24} height={24} />
           <p className="text-xl leading-normal font-montserrat text-slate-gray">
-            (4.5)
+            ({rating})
           </p>
         </div>
-        <h3 className="mt-2 text-2xl font-semibold leading-normal font-palanquin">
+        <h3 className="mt-2 text-2xl font-semibold leading-normal font-palanquin text-primary">
           {name}
         </h3>
-        <p className="mt-2 text-2xl font-semibold leading-normal font-montserrat text-coral-red">
+        <p className="mt-2 text-2xl font-semibold leading-normal font-montserrat text-secondary">
           {price}
         </p>
       </div>
