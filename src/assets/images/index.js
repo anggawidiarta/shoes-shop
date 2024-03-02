aa156ea13cb5b4b4a8275173942ac4036ad3648c
Angga Widiarta <anggawidiarta55@gmail.com>
2024-03-03 07:24:02 +0800

    push

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
+// import logo from "./logo.svg";
 import bigShoe1 from "./big-shoe1.png";
 import bigShoe2 from "./big-shoe2.png";
 import bigShoe3 from "./big-shoe3.png";
@@ -34,6 +35,7 @@ import thumbnailShoe2 from "./thumbnail-shoe2.svg";
 import thumbnailShoe3 from "./thumbnail-shoe3.svg";
 
 export {
+
   eiger,
   adidas,
   converse,
